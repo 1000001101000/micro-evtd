ddc4853be1196fbc1ec4a9688f0679cbde450b6f
1000001101000 <38921757+1000001101000@users.noreply.github.com>
2019-12-16 17:44:36 -0600

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+version="7.5-2019.12"
+
+wget "https://releases.linaro.org/components/toolchain/binaries/$version/arm-linux-gnueabihf/gcc-linaro-$version-x86_64_arm-linux-gnueabihf.tar.xz
+tar xf gcc-linaro-$version-x86_64_arm-linux-gnueabihf.tar.xz
+
+mkdir bins
+make 
+if [ $? -ne 0 ]; then
+  echo "build failed"
+  exit 99
+fi
+cp bin/micro-evtd bins/micro-evtd-x86
+make clean
+prefix="gcc-linaro-$version-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
+make CROSS_COMPILE="$prefix"
+if [ $? -ne 0 ]; then
+  echo "build failed"
+  exit 99
+fi
+cp bin/micro-evtd bins/micro-evtd-armhf
+find .
